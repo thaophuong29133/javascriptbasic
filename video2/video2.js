52430402ba6b1140faa07a5d833b40e44223bd78
Phuong Ngo <thaophuong2812@gmail.com>
2024-06-17 11:46:27 +0700

    js basic 2

--- video2/video2.js
+++ video2/video2.js
@@ -1,1 +1,27 @@
 console.log('Hello world from javascript')
+let y = 5, z = 6;
+console.log(`My name's Phuong. It's amazing`)
+const x = 7
+console.log(x)
+console.log("Test", 'x=', x, 'y=', y, 'z=', z)
+console.log("Test" + 'x=' + x + 'y=' + y + 'z=' + z)
+let text = "qưertyuiop"
+console.log(text.length);
+console.log(`hoi dan it check string a = ${x} ,  b = ${y}`)
+let obj = {
+    name: "Phuong",
+    age: 29
+};
+console.log(`My name's`, obj.name)
+JSON
+"{name: 'Phuong', age: '29'}"
+console.log(`check object`, obj)
+
+let d = ['cat', 'dog', 'mouse']
+console.log(`check type and value`, typeof d, d)
+
+let family = [
+    { name: 'Phuong', age: 29 },
+    { name: 'Phong', age: 31 }
+]
+console.log(`my family`, family)
