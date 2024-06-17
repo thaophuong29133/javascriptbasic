741f0029b73fc55e407ef7015113d235f690c744
Phuong Ngo <thaophuong2812@gmail.com>
2024-06-17 17:50:06 +0700

    js basic 3

--- video2/video2.js
+++ video2/video2.js
@@ -25,3 +25,18 @@ let family = [
     { name: 'Phong', age: 31 }
 ]
 console.log(`my family`, family)
+
+let e = 5
+let f = '5'
+console.log(`compare: `, 'e =', e, 'f =', f, e === f)
+
+let arr = ['MU', 'Chelsea', 'Liver', 'Mancity']
+for (let i = 0; i < arr.length; i++) {
+    console.log(`check variables:`, arr[i])
+}
+
+
+let arrTop4 = ['Phuong', 'Phong', 'Rin', 'Chung']
+for (let i = 0; i < 4; i++) {
+    console.log('Xinh nhat nha', i + 1, arrTop4[i])
+}
